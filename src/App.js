70eaa1d805061e5339a8cf9e11d105b067380ba5
Francislainy Campos <francislainy.campos@hmhco.com>
2020-08-27 04:05:52 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        My Hello World
+        <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent />
+      </div>
+    );
+  }
+}
+
+// Class Component
+class FirstComponent extends Component {
+  render() {
+    return (
+      <div className="firstComponent">
+        First Component
+      </div>
+    );
+  }
+}
+
+class SecondComponent extends Component {
+  render() {
+    return (
+      <div className="secondComponent">
+        Second Component
+      </div>
+    );
+  }
+}
+
+function ThirdComponent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="thirdComponent">
+      Third Component
     </div>
   );
 }
+
+
+// class ThirdComponent extends Component {
+//   render() {
+//     return (
+//       <div className="thirdComponent">
+//         Third Component
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
